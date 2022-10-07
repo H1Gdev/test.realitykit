b435b301d279ba97a2d1c7e4ad78bea87748f43c
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2022-10-07 17:15:09 +0900

    Add ARCoachingOverlayView.

--- test.realitykit/ARViewContainer.swift
+++ test.realitykit/ARViewContainer.swift
@@ -7,11 +7,13 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ARViewContainer: UIViewRepresentable {
 
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero)
+        addCoachingOverlay(arView: arView)
 
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
@@ -23,4 +25,20 @@ struct ARViewContainer: UIViewRepresentable {
     }
 
     func updateUIView(_ uiView: ARView, context: Context) {}
+
+    private func addCoachingOverlay(arView: ARView) {
+        let arCoachingOverlayView = ARCoachingOverlayView()
+        arCoachingOverlayView.session = arView.session
+        arCoachingOverlayView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        arCoachingOverlayView.goal = .horizontalPlane
+        arView.addSubview(arCoachingOverlayView)
+    }
+
+    private func removeCoachingOverlay(arView: ARView) {
+        for subview in arView.subviews {
+            if let arCoachingOverlayView = subview as? ARCoachingOverlayView {
+                arCoachingOverlayView.removeFromSuperview()
+            }
+        }
+    }
 }
